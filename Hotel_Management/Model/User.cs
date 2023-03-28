10aca8fd6ebad59509718e53805d0fbcb459ceb7
Abhishek <abhishekopm999@gmail.com>
2023-03-28 22:54:32 +0530

    Add project files.

---
+++ Hotel_Management/Model/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Hotel_Management.Model
+{
+    public class User
+    {
+        public int ClientId { get; set; }
+        public string ClientFirstName { get; set; }
+        public string ClientLastName { get; set; }
+        // public Gender genger { get; set; }
+        public int ClientAge { get; set; }
+        public string ClientPhoneNumber { get; set; }
+        public string ClientEmail { get; set; }
+        public string ClientPassword { get; set; }
+        // public Role role { get; set; }
+
+        //public  Address address {get;set;}
+
+        //List<Booking> bookings {get;set;}
+    }
+}
